212ac0844d9c7f91279734ee10163dfc5d3339dc
mrfoxpro <tuningiposadka@gmail.com>
2025-03-30 13:54:11 +0500

    specify tag for ChatMember enum

--- tg/schema.rs
+++ tg/schema.rs
@@ -1776,7 +1776,7 @@ impl ChatLocation {
 
 https://core.telegram.org/bots/api/#chatmember*/
 #[derive(Clone, Debug, Deserialize, From)]
-#[serde(tag = "status")]
+#[serde(tag = "status", rename_all = "snake_case")]
 pub enum ChatMember {
 	Administrator(ChatMemberAdministrator),
 	Banned(ChatMemberBanned),
